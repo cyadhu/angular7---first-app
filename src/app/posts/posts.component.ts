374de735d1aa37fab554671433026204e0b7b6e5
Yadhu Krishnan <yadhu.krishnan@fingent.com>
2018-10-29 18:53:24 +0530

    first app

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+  posts : object;
+  constructor(private data : DataService) { }
+
+  ngOnInit() {
+  	this.data.getPosts().subscribe(
+  		postData => this.posts = postData
+  	);
+  }
+
+}
